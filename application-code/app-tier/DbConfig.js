84262999412c7ea6daacd6e42cf3b1e05e9619dc
ShirishaPatil08 <161926477+ShirishaPatil08@users.noreply.github.com>
2026-04-16 21:29:42 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'database-1.c380a08uukyc.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'database-1.us-east-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'kastro2025',
+    DB_PWD : 'Adminadmin',
     DB_DATABASE : 'webappdb'
 });
